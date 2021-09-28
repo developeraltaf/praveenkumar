7fb2bd66fbe6d99487f72215aa2c6d549a3b3faf
developeraltaf <71233503+developeraltaf@users.noreply.github.com>
2021-09-28 16:28:36 +0530

    khalid

--- src/markup/Element/ThemeButton/ThemeButton.js
+++ src/markup/Element/ThemeButton/ThemeButton.js
@@ -4,8 +4,7 @@ export default class ThemeButton extends Component{
 	render(){
 		return(
 			<>
-				<a href="https://1.envato.market/3zZ9y" className="bt-buy-now theme-btn"><i className="ti-shopping-cart"></i><span>Buy Now</span></a>
-				<a href="https://w3itexperts.ticksy.com"  className="bt-support-now theme-btn"><i className="ti-headphone-alt"></i><span>Support</span></a>
+				<a href="https://wa.me/+919102603030"  className="bt-support-now theme-btn"><i className="ti-headphone-alt"></i><span>WhatsApp</span></a>
 			</>
 		)
 	}	
